bbcb4f9cba2d132fbb2af3d139ff0f6c5e0f1b8c
Lazar Kostić <lazar@kostic.email>
2025-05-18 14:23:20 +0200

    Initial commit - necessary to ensure proper functioning of Organizer classes

---
+++ organizers/__init__.py
@@ -0,0 +1,6 @@
+# organizers/__init__.py
+
+from .DateOrganizer import DateOrganizer
+from .SizeOrganizer import SizeOrganizer
+from .NameOrganizer import NameOrganizer
+from .ExtensionOrganizer import ExtensionOrganizer
